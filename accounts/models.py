aa3c8fbb6153ac4ddb53c99adea6353a4c7ffee0
Ehtishamul Huq <ehtishamulhuq7@gmail.com>
2021-05-20 16:02:51 +0530

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import AccountManager
+# Create your models here.
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+
+    objects = AccountManager()
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self,app_label):
+        return True
